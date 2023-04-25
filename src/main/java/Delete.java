9ddee865f0d440795aaeb7fedcdf768f79e785d0
Sasmita <sasmitapradhan01@gmail.com>
2023-04-25 12:08:00 -0400

    Commit delete

---
+++ src/main/java/Delete.java
@@ -0,0 +1,15 @@
+import java.io.File;
+
+class Delete{
+	public static void main(String[] args){
+		//creates a file object
+		File file = new File("file.txt");
+
+		//deletes the file
+		boolean value = file.delete();
+		if(value)
+			System.out.println("The File is deleted.");
+		else
+			System.out.println("The File is not deleted.");
+	}
+}
