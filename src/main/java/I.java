fb7af7ab8a52609a27924650c230a186bdfbcc96
Sasmita <sasmitapradhan01@gmail.com>
2023-04-25 14:25:42 -0400

    Commit

---
+++ src/main/java/I.java
@@ -0,0 +1,11 @@
+class I{
+	public static void main(String[] args){
+		//create string type variable
+		String data = "10";
+		System.out.println("The string value is: " + data);
+
+		//convert String variable to int
+		int num = Integer.parseInt(data);
+		System.out.println("The integer value is: " + num);
+	}
+}
